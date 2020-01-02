f2fe93e7f1784372c9ae26f3401a0c9a80afe0b0
ivarprudnikov <ivar.prudnikov@gmail.com>
2020-01-02 17:15:46 +0000

    Add routes and update NavBar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
 import NavBar from "./components/NavBar";
-import { useAuth0 } from "./react-auth0-spa";
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
 
 function App() {
-
-  const { loading } = useAuth0();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
