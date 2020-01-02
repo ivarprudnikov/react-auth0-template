50b1cd7f523dc112208b83f021c0c4113db32b00
ivarprudnikov <ivar.prudnikov@gmail.com>
2020-01-02 17:20:12 +0000

    Protext profile route with PrivateRoute component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import { Router, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile";
 import history from "./utils/history";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         </header>
         <Switch>
           <Route path="/" exact />
-          <Route path="/profile" component={Profile} />
+          <PrivateRoute path="/profile" component={Profile} />
         </Switch>
       </Router>
     </div>
