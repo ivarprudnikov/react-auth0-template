67d0759bf751a85c66a7f6196d327e2bdf8680e6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-19 12:55:01 +0000

    Deploy ivarprudnikov/react-auth0-template to github.com/ivarprudnikov/react-auth0-template.git:gh-pages

---
+++ precache-manifest.287bd288496b9e727dd7df28baa4e0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d827c43bb415c75630208e896dea8fb2",
+    "url": "/react-auth0-template/index.html"
+  },
+  {
+    "revision": "495e02255bf811fd9e83",
+    "url": "/react-auth0-template/static/css/main.1425ad0d.chunk.css"
+  },
+  {
+    "revision": "8f7d02ce871938e3a39c",
+    "url": "/react-auth0-template/static/js/2.2da9e8b6.chunk.js"
+  },
+  {
+    "revision": "4c550ccd2897bfa07816b0d6a68ede66",
+    "url": "/react-auth0-template/static/js/2.2da9e8b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495e02255bf811fd9e83",
+    "url": "/react-auth0-template/static/js/main.738e9f06.chunk.js"
+  },
+  {
+    "revision": "485b74a6a93b112b07e7",
+    "url": "/react-auth0-template/static/js/runtime-main.f2b5d3e9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-auth0-template/precache-manifest.0705cfb0e80c49df40cc3fd61c20b936.js"
+  "/react-auth0-template/precache-manifest.287bd288496b9e727dd7df28baa4e0a5.js"
 );
 
 self.addEventListener('message', (event) => {
