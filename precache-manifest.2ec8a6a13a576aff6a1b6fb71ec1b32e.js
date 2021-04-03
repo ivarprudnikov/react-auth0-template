5498f73665202bb88d71085bcc22a1f922190d20
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-03 18:29:08 +0000

    Deploy ivarprudnikov/react-auth0-template to github.com/ivarprudnikov/react-auth0-template.git:gh-pages

---
+++ precache-manifest.2ec8a6a13a576aff6a1b6fb71ec1b32e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c730cf9cbdfb4cc8e3fb64743946463a",
+    "url": "/react-auth0-template/index.html"
+  },
+  {
+    "revision": "dfd1590c35d078d68dba",
+    "url": "/react-auth0-template/static/css/main.a88e7daa.chunk.css"
+  },
+  {
+    "revision": "07c12502e2f19b51312a",
+    "url": "/react-auth0-template/static/js/2.690b7c4c.chunk.js"
+  },
+  {
+    "revision": "ffe342c43eb1bc2ccc41fe4aa32f5ad5",
+    "url": "/react-auth0-template/static/js/2.690b7c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd1590c35d078d68dba",
+    "url": "/react-auth0-template/static/js/main.8187abe1.chunk.js"
+  },
+  {
+    "revision": "485b74a6a93b112b07e7",
+    "url": "/react-auth0-template/static/js/runtime-main.f2b5d3e9.js"
+  }
+]);
