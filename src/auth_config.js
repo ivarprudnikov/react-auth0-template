5ef7201369c4cc554db6d1eb6ad6ed25618ea650
ivarprudnikov <ivar.prudnikov@gmail.com>
2021-04-05 14:21:23 +0100

    eslint

--- src/auth_config.js
+++ src/auth_config.js
@@ -1,10 +1,12 @@
 import packageJson from '../package.json';
 
-export default {
+const auth_config = {
   domain: "ivarprudnikov.eu.auth0.com",
   clientId: "EuHrJlslqdI6h5UOuAMtau6maPa05lRd",
   audience: "https://ivarprudnikov.com",
   scope: "openid profile email",
   loginCallbackUrl: process.env.NODE_ENV === 'production' ? packageJson.homepage : window.location.origin,
   logoutRedirectUrl: process.env.NODE_ENV === 'production' ? packageJson.homepage : window.location.origin
-}
+};
+
+export default auth_config;
